7f0c81dd1a9037d93be65ed44f5e83049df12ef6
Tony Gorez <gorez.tony@gmail.com>
2026-03-03 11:25:36 +0100

    fixes
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const os = require('os')
 
-const platform = os.platform()
-
-let mod
-
-switch (platform) {
+switch (os.platform()) {
   case 'darwin':
   case 'ios':
-    mod = require('./lib/apple.js')
+    module.exports = require('./lib/apple.js')
     break
   case 'linux':
-    mod = require('./lib/linux.js')
+    module.exports = require('./lib/linux.js')
     break
   case 'android':
-    mod = require('./lib/android.js')
+    module.exports = require('./lib/android.js')
     break
   case 'win32':
-    mod = require('./lib/win32.js')
+    module.exports = require('./lib/win32.js')
     break
   default:
     throw new Error(`Unsupported platform: ${platform}`)
 }
-
-exports.persistent = mod.persistent
-exports.ephemeral = mod.ephemeral
